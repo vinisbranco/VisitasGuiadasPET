0b04328f905fea8a2184139f2e03ded606160313
ViniciusCT <vinicius_ct@outlook.com.br>
2017-10-04 16:14:02 -0300

    Teste firebase

--- app/src/main/java/com/example/vinicius/visitasguiadaspet/ActivityConteudo.java
+++ app/src/main/java/com/example/vinicius/visitasguiadaspet/ActivityConteudo.java
@@ -2,12 +2,29 @@ package com.example.vinicius.visitasguiadaspet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ActivityConteudo extends AppCompatActivity {
+
+    TextView txtNome;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conteudo);
+
+        txtNome = (TextView) findViewById(R.id.txtNome);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("Entidades").child("1").child("nome");
+
+        final String nome = mDatabase.push().getKey();
+
+        txtNome.setText(nome);
+
+
     }
 }
